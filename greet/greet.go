4eec4f21cd5f0ba6d27906aa10f8c291c1554cc8
iceluv666 <friendlyneighbor666@gmail.com>
2024-05-21 14:16:26 +0500

    as

--- greet/greet.go
+++
@@ -1,9 +0,0 @@
-package greet
-
-import (
-	"fmt"
-)
-
-func Greet(name string, table int) string {
-	return fmt.Sprintf("Привет, %s! Your table is %d.", name, table)
-}
--- main.go
+++ main.go
@@ -2,12 +2,63 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
-	"github.com/iceluv666/iceles/greet"
+	tables "github.com/iceluv666/iceles/tables"
 )
 
 func main() {
-	text := greet.Greet("Sasha", 5)
+	// Get user information
+	name, location, tableNumber := getUserInput()
 
-	fmt.Println(text)
+	// Reserve table
+	reserved, message := tables.ReserveTable(name, location, tableNumber)
+
+	// Print confirmation message
+	fmt.Println(message)
+
+	if reserved {
+		fmt.Printf("Столик №%d (%s) забронирован на имя %s.\n", tableNumber, location, name)
+	}
+}
+
+func getUserInput() (string, string, int) {
+	var name, location string
+	var tableNumber int
+
+	fmt.Print("Введите ваше имя: ")
+	fmt.Scanln(&name)
+
+	for {
+		fmt.Print("Выберите место (внутри/снаружи): ")
+		fmt.Scanln(&location)
+
+		if location == "внутри" || location == "снаружи" {
+			break
+		}
+
+		fmt.Println("Неверный ввод. Пожалуйста, введите 'внутри' или 'снаружи'.")
+	}
+
+	for {
+		fmt.Printf("Выберите номер столика (%s): ", location)
+		fmt.Scanln(&input)
+
+		number, err := strconv.Atoi(input)
+		if err != nil {
+			fmt.Println("Неверный ввод. Пожалуйста, введите номер столика.")
+			continue
+		}
+
+		tables := tables.GetTables(location)
+		if number < 1 || number > len(tables) {
+			fmt.Printf("Столика с номером %d не существует. Пожалуйста, выберите другой номер.\n", number)
+			continue
+		}
+
+		tableNumber = number
+		break
+	}
+
+	return name, location, tableNumber
 }
